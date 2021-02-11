0231a17af30f0c332c760dee5016a1d6d4d55410
Dale Myers <dalemy@microsoft.com>
2021-02-11 15:48:54 +0000

    Fix ordering for xcobjects

--- xcodeproj/xcobjects.py
+++ xcodeproj/xcobjects.py
@@ -7,32 +7,6 @@ import deserialize
 from .pbxobject import PBXObject
 
 
-@deserialize.key("build_configuration_ids", "buildConfigurations")
-@deserialize.key("default_configuration_is_visible", "defaultConfigurationIsVisible")
-@deserialize.key("default_configuration_name", "defaultConfigurationName")
-@deserialize.parser("defaultConfigurationIsVisible", lambda x: {"0": False, "1": True}[x])
-@deserialize.downcast_identifier(PBXObject, "XCConfigurationList")
-class XCConfigurationList(PBXObject):
-    """Represents an XCConfigurationList.
-
-    This is a list of build configurations. Usually associated with a target.
-    """
-
-    build_configuration_ids: List[str]
-    default_configuration_is_visible: bool
-    default_configuration_name: str
-
-    def build_configurations(self) -> List[XCBuildConfiguration]:
-        """Get all the build configurations for this list.
-
-        :returns: The build configurations
-        """
-        return [
-            cast(XCBuildConfiguration, self.objects()[build_configuration_id])
-            for build_configuration_id in self.build_configuration_ids
-        ]
-
-
 @deserialize.key("base_configuration_reference_id", "baseConfigurationReference")
 @deserialize.key("build_settings", "buildSettings")
 @deserialize.downcast_identifier(PBXObject, "XCBuildConfiguration")
@@ -57,3 +31,29 @@ class XCBuildConfiguration(PBXObject):
             return None
 
         return cast(XCBuildConfiguration, self.objects()[self.base_configuration_reference])
+
+
+@deserialize.key("build_configuration_ids", "buildConfigurations")
+@deserialize.key("default_configuration_is_visible", "defaultConfigurationIsVisible")
+@deserialize.key("default_configuration_name", "defaultConfigurationName")
+@deserialize.parser("defaultConfigurationIsVisible", lambda x: {"0": False, "1": True}[x])
+@deserialize.downcast_identifier(PBXObject, "XCConfigurationList")
+class XCConfigurationList(PBXObject):
+    """Represents an XCConfigurationList.
+
+    This is a list of build configurations. Usually associated with a target.
+    """
+
+    build_configuration_ids: List[str]
+    default_configuration_is_visible: bool
+    default_configuration_name: str
+
+    def build_configurations(self) -> List[XCBuildConfiguration]:
+        """Get all the build configurations for this list.
+
+        :returns: The build configurations
+        """
+        return [
+            cast(XCBuildConfiguration, self.objects()[build_configuration_id])
+            for build_configuration_id in self.build_configuration_ids
+        ]
